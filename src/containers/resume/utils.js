5279fc8b7ada4b9ada85c45324c2bd5ea92eee65
Harshit Saxena <118069394+HarshitSaxena08@users.noreply.github.com>
2025-12-17 18:23:20 +0530

    Update utils.js

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -1,50 +1,56 @@
 export const data = {
-    experience: [
-      {
-        title: "Web Developer ",
-        subTitle: ", Protecon Solutions pvt ltd ",
-        description:
+  experience: [
+    {
+      title: "Frontend Developer ",
+      subTitle: ", Buzzz Booster pvt ltd ",
+      description:
+        " Designs, writes, and edits website content while ensuring understanding of UI, cross-browser compatibility, and general web functions and standards. Develops solutions for identified problems or bugs, and maintains daily communication with colleagues, managers, and stakeholders. Executes assignments using web applications, scripts, and programming languages such as HTML, CSS, JavaScript, and APIs."
+    },
+    {
+      title: "Web Developer ",
+      subTitle: ", Protecon Solutions pvt ltd ",
+      description:
         " I work here for 6 months as an web Developer and have Web Developer proficient in HTML, CSS, Bootstrap, JavaScript, React JS, and basic Java for Automation Testing. ",
-        date: "Jan 2023 - July 2023",
-      
-      },
-      {
-        title: "Web Developer Intern",
-        subTitle: ", Internshala",
-        description:
-          "Its a 8 weeks Internship program as Web Developer Intern and gain more expertise in the field of development.",
-        
-          date: "Sep 2022 - Nov 2022"
-      
-        },
-      
-    ],
-    education: [
-      {
-        title: "Bachelor of Technology in Computer Science & Engineering",
-        // subTitle: ", Shri Siddhi Vinayak Institute Of Technology",
-        description:
-          "Shri Siddhi Vinayak Institute Of Technology ",
-          date: "2019 - 2023",
-        scored : "Scored 78%",
-      },
-      {
-        title: "Intermediate Schooling",
-        // subTitle: ", L.B.V.M Sr. Sec. School",
-        description:
-        "L.B.V.M Sr. Sec. School",
-          // "12th Grade in Science (PCM)",
-        date: "2018 - 2019",
-        scored : "Scored 64%",
-      },
-      {
-        title: "Secondary Schooling",
-        // subTitle: ",  L.B.V.M Sr. Sec. School",
-        description:
-          "L.B.V.M Sr. Sec. School ",
-        date: "2016 - 2017",
-        scored : "Scored 88%",
+      date: "Jan 2023 - July 2023",
 
-      },
-    ],
-  };
+    },
+    {
+      title: "Web Developer Intern",
+      subTitle: ", Internshala",
+      description:
+        "Its a 8 weeks Internship program as Web Developer Intern and gain more expertise in the field of development.",
+
+      date: "Sep 2022 - Nov 2022"
+
+    },
+
+  ],
+  education: [
+    {
+      title: "Bachelor of Technology in Computer Science & Engineering",
+      // subTitle: ", Shri Siddhi Vinayak Institute Of Technology",
+      description:
+        "Shri Siddhi Vinayak Institute Of Technology ",
+      date: "2019 - 2023",
+      scored: "Scored 78%",
+    },
+    {
+      title: "Intermediate Schooling",
+      // subTitle: ", L.B.V.M Sr. Sec. School",
+      description:
+        "L.B.V.M Sr. Sec. School",
+      // "12th Grade in Science (PCM)",
+      date: "2018 - 2019",
+      scored: "Scored 64%",
+    },
+    {
+      title: "Secondary Schooling",
+      // subTitle: ",  L.B.V.M Sr. Sec. School",
+      description:
+        "L.B.V.M Sr. Sec. School ",
+      date: "2016 - 2017",
+      scored: "Scored 88%",
+
+    },
+  ],
+};
